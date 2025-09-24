6f2a25faa49a03c7240b146bfdd1765ce104da1d
Vijay <polisett@ualberta.ca>
2025-09-23 18:18:21 -0600

    CHanged

--- app/src/main/java/com/example/canvas/Shape.java
+++ app/src/main/java/com/example/canvas/Shape.java
@@ -3,5 +3,5 @@ package com.example.canvas;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR"
+    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
